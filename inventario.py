05a85631c0dd08de4b1f81fd024251ed0e15a96f
Andres Gutierrez <andresgtzleon@gmail.com>
2026-05-21 11:45:57 -0700

    Primeros avances

--- inventario.py
+++ inventario.py
@@ -2,31 +2,54 @@
 Para desarrollar el problema del inventario.
 
 """
-
+import math
 from MDPs import MDP, iteracion_valor
 
 class Inventario(MDP):
     """
-    Clase que representa un MDP para el problema del camión mágico.
-    
-    Si caminas, avanzas 1 con coso 1
-    Si usas el camion, con probabilidad rho avanzas el doble de donde estabas
-    y con probabilidad 1-rho te quedas en el mismo lugar. Todo con costo 2.
-    
-    El objetivo es llegar a la meta en el menor costo posible
-    
+    MDP para el problema de inventario
+
+    Estado s: inventario neto al final del dia
+    s > 0 -> unidades en almacen
+    s = 0 -> sin stock
+    s < 0 -> backlog
+
+    Accion a: cuantas unidades pedir esa tarde
     """    
     
-    def __init__(self, gama,lambda_, ...): #TODO: Agregar lo que se requiera
-        #TODO: Completar el constructor
-        self.gamma = #TODO: Agregar lo que se requiera
-        self.lambda_ = #TODO: Agregar lo que se requiera
-        self.estados = #TODO: Agregar lo que se requiera
-        #TODO: Agregar lo que se requiera
+    def __init__(self, gama,lambda_, capacidad=20, backlog_max=10,
+                 precio=150, costo_var=80, costo_fijo=40, costo_hold=5,
+                 costo_back=15, margen_perd=70): 
+        #Espacio de estados
+        estados = list(range(-backlog_max, capacidad + 1))
+        super().__init__(estados, gama)
+
+        self.lambda = lambda_
+        self.capacidad = capacidad
+        self.backlog_max = backlog_max
+        self.precio = precio
+        self.costo_var = costo_var
+        self.costo_fijo = costo_fijo
+        self.costo_hold = costo_hold
+        self.costo_back = costo_back
+        self.margen_perd = margen_perd
+        
+        self.s_min = -backlog_max
+        self.s_max = capacidad
+
+        #Truncar la demanda donde la probalbilidad acumulada es -1
+        self.D_max = 0
+        while poisson_pmf(self.D_max, lambda_) > 1e-9:
+            self.D_max += 1
     
     def acciones_legales(self, s):
-        #TODO: Completar este método
-        pass
+        """
+        A(s) = {0, 1, ..., capacidad - s}
+
+        No puede pedir negativo y el almacen no puede recibir mas
+        de lo que cabe
+        """
+        return list(range(0, self.capacidad - s + 1))
     
     def recompensa(self, s, a, s_):
         #TODO: Completar este método
